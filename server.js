7900d3b984ae80c9a9d8d17c8814e78c79d07739
Fiona <fiona.soetrisno@gmail.com>
2019-02-02 03:11:11 -0800

    heheh
    hehehxd

--- server.js
+++ server.js
@@ -5,26 +5,16 @@ const fs = require('fs');
 
 // const login = require('./routes/login')
 const app = express();
-const login = require('./routes/login');
-const register = require('./routes/register');
 
 app.use(express.static(path.join(__dirname, '/client/dist/jam-it')));
 app.use(express.static(path.join(__dirname, '/client/src/app/register/index.js')));
 
 
 app.use('/login', express.static(path.join(__dirname, '/client/dist/jam-it')));
-app.use('/', login);
-
 app.use('/register', express.static(path.join(__dirname, '/client/dist/jam-it')));
-app.use('/', register);
-
 app.use('/home', express.static(path.join(__dirname, '/client/dist/jam-it')));
-// app.use('/', home);
-
 app.use('/findbands', express.static(path.join(__dirname, '/client/dist/jam-it')));
-// app.use('/', findbands);
 app.use('/profile', express.static(path.join(__dirname, '/client/dist/jam-it')));
-// app.use('/', findbands);
 
 const port = process.env.PORT || 8010
 
