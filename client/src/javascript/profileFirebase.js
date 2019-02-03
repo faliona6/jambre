90f801a021abb5cb2dec022498f78a354629c1ff
Fiona <fiona.soetrisno@gmail.com>
2019-02-02 18:53:58 -0800

    Fixed Database Push
    Fixed Database Push

--- client/src/javascript/profileFirebase.js
+++ client/src/javascript/profileFirebase.js
@@ -3,16 +3,17 @@ $( document ).ready(function() {
 });
 
 function getUserData() {
-    var user = firebase.auth().currentUser;
-    var docRef = db.collection("users").doc(user);
+    // var user = firebase.auth().currentUser;
+    // var db = firebase.firestore();
+    // var docRef = db.collection("users").doc(user);
 
-    docRef.get().then(function(doc) {
-        if (doc.exists) {
-            var data = console.log(doc.data());
-            console.log(data);
-        }
-        else {
-            console.log("No document");
-        }
-    })
+    // docRef.get().then(function(doc) {
+    //     if (doc.exists) {
+    //         var data = console.log(doc.data());
+    //         console.log(data);
+    //     }
+    //     else {
+    //         console.log("No document");
+    //     }
+    // })
 }
