fc3ef7e68e6a4b55f37c473c1c04fa1df0976833
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2019-02-02 16:24:42 -0800

    fix createband page

--- client/src/app/createband/createband.component.ts
+++ client/src/app/createband/createband.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-createband',
   templateUrl: './createband.component.html',
-  styleUrls: ['./createband.component.css', '../app.component.css']
+  styleUrls: ['./createband.component.css', '../bandpage/bandpage.component.css','../app.component.css']
 })
 export class CreatebandComponent implements OnInit {
 
