1fbfc75bced2b3f429369a3c78b54ce83793d807
Fiona <fiona.soetrisno@gmail.com>
2019-02-02 16:26:39 -0800

    aa
    aa

--- client/src/registerFirebase.js
+++ client/src/registerFirebase.js
@@ -10,21 +10,7 @@ var config = {
   firebase.initializeApp(config);
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      // User is signed in.
-      document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
       var user = firebase.auth().currentUser;
-
-      if(user != null){
-        var email_id = user.email;
-        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-      }
-    }
-
-    else {
-      // No user is signed in.
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
     }
   });
 
